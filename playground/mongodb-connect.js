f190ca056d7610170351ecce8ea8aabe67bcc11b
David A. Foote <david.a.foote@gmail.com>
2019-01-04 19:13:48 -0600

    Add/Delete script

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -19,17 +19,18 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   //   console.log(JSON.stringify(result.ops, undefined, 2));
   // });
 
-    // db.collection('Users').insertOne({
-    //   name: 'David',
-    //   age: 55,
-    //   location: 'Saint Louis'
-    // }, (err, result) => {
-    //   if (err) {
-    //     return console.log('Unable to insert user', err);
-    //   }
-    //
-    //   console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
-    // });
+    db.collection('Users').insertOne({
+      name: 'David',
+      age: 55,
+      location: 'Saint Louis'
+    }, (err, result) => {
+      if (err) {
+        return console.log('Unable to insert user', err);
+      }
+
+      //console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+      console.log(result.ops);
+    });
 
 
 
