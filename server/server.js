a5b489fc450e8adb9fff1318961493465eae1911
David A. Foote <david.a.foote@gmail.com>
2019-01-12 02:12:38 -0600

    Added GET /todos route

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.post("/todos", (req, res) => {
+app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
   });
@@ -19,6 +19,14 @@ app.post("/todos", (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
 });
 
 app.listen(3000, () => {
